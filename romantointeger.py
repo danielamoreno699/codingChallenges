3e1c85db460fb0b19e0b4f3e94243cbbb2a73505
Daniela Moreno <dani9292@hotmail.com>
2023-10-04 12:42:23 -0500

    update code

--- romantointeger.py
+++ romantointeger.py
@@ -1,13 +1,15 @@
 def romanToInt(s):
-        dic = {'I': 1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    value = 0
 
-        print(s)
-        for i in reversed(s):
-            if i in dic:
-                 
-                  print(f"The value for '{i}' is: {dic[i]}")
+    for i in reversed(s):
+        if i in dic:
+            if value > 0 and dic[i] < dic[s[s.index(i) + 1]]:
+                value -= dic[i]
             else:
-                  -1
+                value += dic[i]
 
-print(romanToInt('VI'))
-print(romanToInt('IV'))
+    return value
+
+print(romanToInt('VI'))  # Should print 6
+print(romanToInt('IV'))  # Should print 4
