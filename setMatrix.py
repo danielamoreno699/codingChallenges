df96f229994188e299b5e07cdadd398790f9d1ca
Daniela Moreno <dani9292@hotmail.com>
2023-10-05 20:05:00 -0500

    update code

--- setMatrix.py
+++ setMatrix.py
@@ -4,14 +4,15 @@ def printZeroPositions(matrix):
 
     for i in range(n):
         for j in range(m):
-            print(matrix[i][j], end=" ")
+            print( matrix[i][j], end=" ")
            
-        print()  
+        print('old matrix')  
 
     for i in range(n):
         for j in range(m):
             if matrix[i][j] == 0:
                 print(f'Position ({i}, {j}) has a zero')
+                matrix[i] == 0 and matrix[j] == 0
 
 matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
 printZeroPositions(matrix)
