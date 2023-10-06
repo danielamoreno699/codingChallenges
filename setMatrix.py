fc9b27c13d1ae71ba4f765f60dcb31be556f74bc
Daniela Moreno <dani9292@hotmail.com>
2023-10-05 20:33:34 -0500

    update code

--- setMatrix.py
+++ setMatrix.py
@@ -2,10 +2,7 @@ def printZeroPositions(matrix):
     n = len(matrix)
     m = len(matrix[0])
 
-    #test[:, 0] select col from index 0
-    #test[0, :] select row from index 0
-
-
+  
     for i in range(n):
         for j in range(m):
             print( matrix[i][j], end=" ")
@@ -16,7 +13,11 @@ def printZeroPositions(matrix):
         for j in range(m):
             if matrix[i][j] == 0:
                 print(f'Position ({i}, {j}) has a zero')
-                matrix[i] == 0 and matrix[j] == 0
+                matrix[i, :] = 0 # assign row vector to value zero
+                matrix[:, j] = 0 # assign col vector to value zero
+                
+        
+       
 
 matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
 printZeroPositions(matrix)
