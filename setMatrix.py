34c43a3509811bb6a53c205f1dcd9e5e6a7c55fe
Daniela Moreno <dani9292@hotmail.com>
2023-10-05 19:41:38 -0500

    update code

--- setMatrix.py
+++ setMatrix.py
@@ -1,9 +1,20 @@
 def setZeroes(list):
-    print(list)
+   
 
     #find zero
     #set the col and rows to 0
     #modify the matrix
+    n = 3
+    m = 3
+    
+    for i in range(0, n):
+ 
+        for j in range(0, m):
+ 
+            # Printing jth element of ith row
+            print(list[i][j], end=" ")
+    
+        print()
  
 print(setZeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]]))
        
