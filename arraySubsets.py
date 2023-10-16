95647a45ed65e0e87d4a74ee2e565b281e7b38bb
Daniela Moreno <dani9292@hotmail.com>
2023-10-16 11:51:36 -0500

    update code

--- arraySubsets.py
+++ arraySubsets.py
@@ -1,19 +1,33 @@
+from itertools import combinations
+
 def findMax(arr):
     subsetA = []
-    max_value = float('-inf')
-    
-    for i in arr:
-        if i > max_value:
-            max_value = i
-            subsetA.append(max_value)
+    subsetB = []
+    sorted_arr_desc = sorted(arr, reverse=True)
 
-    for i in arr:
-        if i == max_value:
-            print(f"{i} is the maximum value in the array.")
+    # for i in range(len(sorted_arr_desc) - 1):
+    #     subsetA.append([sorted_arr_desc[i], sorted_arr_desc[i + 1]])
+    for i in range(len(sorted_arr_desc)):
+        for j in range(i + 1, len(sorted_arr_desc)):
+            pair = [sorted_arr_desc[i], sorted_arr_desc[j]]
+            subsetA.append(pair)
+
+    for subA in subsetA:
+        exclusion = [element for element in arr if element not in subA]
+        subsetB.append(exclusion)
+
+    for i in range(len(subsetA)):
+        subA = subsetA[i]
+        subB = subsetB[i]
+        print("SubsetA:", subA)
+        print("SubsetB:", subB)
+
+        if sum(subA) > sum(subB):
+            print('subA', subA)
+            print('SubsetA is greater')
+            return subA
+
         else:
-            print(f"{i} is not the maximum value in the array.")
-    
-    return subsetA
+            print('SubsetA is not greater')
 
-subsetA = findMax([3, 7, 5, 6, 2])
-print("SubsetA:", subsetA)
+findMax([3, 7, 5, 6, 2])
