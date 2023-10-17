74d95d55187d9a47022db829d62f0e0a58df8898
Daniela Moreno <dani9292@hotmail.com>
2023-10-17 13:22:55 -0500

    update code

--- 3sum.py
+++ 3sum.py
@@ -18,3 +18,28 @@
 
 # result = sum([-1, 2, 1, -4], 1)
 # print(result)
+def threeSumClosest(nums, target):
+    nums.sort()  # Sort the input list first
+    closest = float('inf')  # Initialize closest with positive infinity
+    result = 0
+
+    for i in range(len(nums) - 2):
+        left = i + 1
+        right = len(nums) - 1
+
+        while left < right:
+            current_sum = nums[i] + nums[left] + nums[right]
+            distance = current_sum - target
+
+            if abs(distance) < abs(closest):
+                closest = distance
+                result = current_sum
+
+            if current_sum < target:
+                left += 1
+            else:
+                right -= 1
+
+    return result
+
+print(threeSumClosest([-1, 2, 1, -4], 1))
